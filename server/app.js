8e56111c4b4f44d1e26cdef58b6ee1e9a1cc5087
Yimin Yang <yvonne.yangh@gmail.com>
2023-06-28 13:55:58 -0500

    fix script for autocomplete in index.html

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const path = require('path');
 app.use(express.json());
+app.engine('html',require('ejs').renderFile);
 
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 app.use('/static', express.static(path.join(__dirname, '../static')));
 
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../static/index.html')));
+//app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../static/index.html')));
+app.get('/', (req, res)=> res.render(path.join(__dirname, '../static/index.html'),{
+    REACT_APP_GOOGLE_MAPS_API_KEY:process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+}));
 
 app.use('/api/auth', require('./api/auth'));
 app.use('/api/orders', require('./api/orders'));
