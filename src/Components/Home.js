a35fd5d9b6cf953cf31935dfb75a80b478875198
Yimin Yang <yvonne.yangh@gmail.com>
2023-07-04 10:01:45 -0500

    connect home favorite button with favorite list

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../store";
+import Favorite from "./Favorite";
 
 const Home = () => {
   const { auth } = useSelector((state) => state);
@@ -12,6 +13,7 @@ const Home = () => {
   //search bar: handle the search and convert it to URL Parameter.
   const [searchInput, setSearchInput] = useState("");
   const [selectedPlace, setSelectedPlace] = useState(null);
+  const [isFavoriteOpen,setIsFavoriteOpen] = useState(false);
 
   const onChange = (ev) => {
     setSearchInput(ev.target.value);
@@ -30,6 +32,15 @@ const Home = () => {
 
   const handleMyLocation = () => {
     navigate(`/map/place/${encodeURIComponent("nearby")}`);
+  };
+
+  // my favorite module
+  const openMyFavorite = () => {
+    setIsFavoriteOpen(true);
+  };
+
+  const closeMyFavorite = () => {
+    setIsFavoriteOpen(false);
   };
 
   useEffect(() => {
@@ -53,68 +64,75 @@ const Home = () => {
   }, [elPlace.current]);
 
   return (
-    <div className="container d-flex flex-column align-items-center justify-content-center">
-      <div className="my-5 py-5">
-        <div className="text-center mb-4">
-          <img
-            src="https://cdn1.iconfinder.com/data/icons/game-design-volume-2/512/Quest-512.png"
-            alt="Quest Icon"
-            className="mb-2"
-            style={{ width: "200px", height: "200px" }}
-          />
-          <h1 className="mb-4">Begin your Quest</h1>
-          <form onSubmit={handleSearch} className="d-flex mb-4">
-            <input
-              type="text"
-              className="form-control shadow-sm me-2 mb-1"
-              placeholder="Search city, place, or address"
-              value={searchInput}
-              onChange={onChange}
-              ref={elPlace}
-              style={{ width: "400px" }}
+    <>
+      {
+        isFavoriteOpen&&(
+          <Favorite onClose={closeMyFavorite}/>
+        )
+      }
+      <div className="container d-flex flex-column align-items-center justify-content-center">
+        <div className="my-5 py-5">
+          <div className="text-center mb-4">
+            <img
+              src="https://cdn1.iconfinder.com/data/icons/game-design-volume-2/512/Quest-512.png"
+              alt="Quest Icon"
+              className="mb-2"
+              style={{ width: "200px", height: "200px" }}
             />
-            <button type="submit" className="btn btn-dark default-button">
-              Search
-            </button>
-          </form>
-        </div>
-        <div className="d-grid gap-3">
-          <div className="row">
-            <div className="col">
-              <button
-                className="btn btn-dark mb-2 default-button"
-                style={{ width: "200px" }}
-                onClick={handleMyLocation}
-              >
-                Nearby
+            <h1 className="mb-4">Begin your Quest</h1>
+            <form onSubmit={handleSearch} className="d-flex mb-4">
+              <input
+                type="text"
+                className="form-control shadow-sm me-2 mb-1"
+                placeholder="Search city, place, or address"
+                value={searchInput}
+                onChange={onChange}
+                ref={elPlace}
+                style={{ width: "400px" }}
+              />
+              <button type="submit" className="btn btn-dark default-button">
+                Search
               </button>
-            </div>
-            <div className="col">
-              <Link to="/map">
+            </form>
+          </div>
+          <div className="d-grid gap-3">
+            <div className="row">
+              <div className="col">
                 <button
                   className="btn btn-dark mb-2 default-button"
                   style={{ width: "200px" }}
+                  onClick={handleMyLocation}
                 >
-                  Show Map
+                  Nearby
                 </button>
-              </Link>
+              </div>
+              <div className="col">
+                <Link to="/map">
+                  <button
+                    className="btn btn-dark mb-2 default-button"
+                    style={{ width: "200px" }}
+                  >
+                    Show Map
+                  </button>
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <button className="btn btn-dark default-button" style={{ width: "200px" }}>
-                Favorites
-              </button>
-            </div>
-            <div className="col">
-              <button className="btn btn-dark default-button" style={{ width: "200px" }}>
-                Help
-              </button>
+            <div className="row">
+              <div className="col">
+                <button className="btn btn-dark default-button" style={{ width: "200px" }} onClick={openMyFavorite}>
+                  Favorites
+                </button>
+              </div>
+              <div className="col">
+                <button className="btn btn-dark default-button" style={{ width: "200px" }}>
+                  Help
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
