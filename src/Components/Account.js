94838c0d827da2a95ae9f4f3e3b998ffa034dd5e
Phillip Choi <38228979+Minsu386@users.noreply.github.com>
2023-07-07 16:38:23 -0400

    fix error 500 on update vehicle info.BUG cannot read prop of null of id ..undefined

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -174,3 +175,180 @@ const MyAccount = () => {
 };
 
 export default MyAccount;
+
+// import React, { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Link } from "react-router-dom";
+// import { fetchAllUsers, fetchVehicles } from "../store";
+
+// const MyAccount = () => {
+//   const dispatch = useDispatch();
+//   const userAuthObj = useSelector((state) => state.auth);
+//   const usersList = useSelector((state) => state.user.usersList);
+//   const userStatus = useSelector((state) => state.user.status);
+//   const vehicle = useSelector((state) => state.vehicle.vehicles);
+//   const [showPassword, setShowPassword] = React.useState(false);
+
+//   useEffect(() => {
+//     if (userStatus === "idle") {
+//       dispatch(fetchAllUsers());
+//       dispatch(fetchVehicles());
+//     }
+//   }, [dispatch, userStatus]);
+
+//   let content;
+
+//   if (userStatus === "loading") {
+//     content = <div>Loading...</div>;
+//   } else if (userStatus === "succeeded") {
+//     const user = usersList.find((e) => e.id === userAuthObj.id);
+//     console.log("user", user);
+//     console.log("userAuthObj:", userAuthObj);
+//     const userVehicle = vehicle.find((v) => v.userId === userAuthObj.id);
+//     console.log("userVehicle:", userVehicle);
+
+//     content = (
+//       <>
+//         <div className="container-fluid py-5 h-100">
+//           <div className="row d-flex justify-content-center align-items-center h-100">
+//             <div className="col col-lg-6 mb-4 mb-lg-0">
+//               {user && (
+//                 <div className="card mb-3" style={{ borderRadius: ".5rem" }}>
+//                   <div className="row g-0">
+//                     <div
+//                       className="col-md-4 text-center"
+//                       style={{
+//                         borderTopLeftRadius: ".5rem",
+//                         borderBottomLeftRadius: ".5rem",
+//                         backgroundColor: "#779DA6",
+//                       }}
+//                     >
+//                       <img
+//                         className="img-fluid mt-5"
+//                         src={user.avatar}
+//                         alt="User Avatar"
+//                         style={{ width: "80px" }}
+//                       />
+//                       <div className="my-account-details my-5">
+//                         <div>
+//                           <h6 className="my-account-p">Username</h6>
+//                           <p className="text-muted">{user.username}</p>
+//                         </div>
+//                       </div>
+//                     </div>
+//                     <div className="col-md-8">
+//                       <div className="card-body p-4">
+//                         <h6>Information</h6>
+//                         <hr className="mt-0 mb-4" />
+//                         <div className="row pt-1">
+//                           <div className="col-md-6 mt-1">
+//                             <h6 className="my-account-p">Email</h6>
+//                             <p className="text-muted mt-1">{user.email}</p>
+//                           </div>
+//                           <div className="col-md-6">
+//                             <div className="d-flex align-items-center">
+//                               <h6 className="my-account-p">Password</h6>
+//                               <button
+//                                 className="btn btn-link mb-2"
+//                                 onClick={() => setShowPassword(!showPassword)}
+//                               >
+//                                 {showPassword ? (
+//                                   <i className="fas fa-eye-slash"></i>
+//                                 ) : (
+//                                   <i className="fas fa-eye"></i>
+//                                 )}
+//                               </button>
+//                             </div>
+//                             {showPassword ? (
+//                               <p className="text-muted">{user.password}</p>
+//                             ) : (
+//                               <input
+//                                 type="password"
+//                                 value={user.password}
+//                                 className="form-control"
+//                                 disabled
+//                               />
+//                             )}
+//                           </div>
+//                           <div className="col-md-6 mt-4">
+//                             <h6 className="my-account-p">Address</h6>
+//                             <p className="text-muted">{user.address}</p>
+//                           </div>
+//                           <div className="col-md-6 mt-5">
+//                             <Link to={`/myaccount/updateuser`}>
+//                               <button className="btn default-button">
+//                                 Update your information
+//                               </button>
+//                             </Link>
+//                           </div>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//               )}
+//               {userVehicle && (
+//                 <div className="card my-3" style={{ borderRadius: ".5rem" }}>
+//                   <div className="row g-0">
+//                   <div
+//                       className="col-md-4 text-center"
+//                       style={{
+//                         borderTopLeftRadius: ".5rem",
+//                         borderBottomLeftRadius: ".5rem",
+//                         backgroundColor: "#779DA6",
+//                       }}
+//                     >
+//                       <img
+//                         className="user-avatar mt-5"
+//                         src={userVehicle.image}
+//                         alt="Vehicle"
+//                         style={{ width: "200px" }}
+//                       />
+//                     </div>
+//                     <div className="card-body p-5">
+//                       <h6>Vehicle Information</h6>
+//                       <hr className="mt-0 mb-4" />
+//                       <div className="row pt-1">
+//                         <div className="col-md-6 mt-1">
+//                           <h6 className="my-account-p">Make</h6>
+//                           <p className="text-muted mt-1">{userVehicle.make}</p>
+//                         </div>
+//                         <div className="col-md-6 mt-1">
+//                           <h6 className="my-account-p">Model</h6>
+//                           <p className="text-muted mt-1">{userVehicle.model}</p>
+//                         </div>
+//                         <div className="col-md-6 mt-1">
+//                           <h6 className="my-account-p">Year</h6>
+//                           <p className="text-muted mt-1">{userVehicle.year}</p>
+//                         </div>
+//                         <div className="col-md-6 mt-1">
+//                           <h6 className="my-account-p">Charger Type</h6>
+//                           <p className="text-muted mt-1">
+//                             {userVehicle.chargertype}
+//                           </p>
+//                         </div>
+//                       </div>
+//                       <div className="col-md-6 mt-5">
+//                         <Link to={`/myaccount/updatevehicle/${vehicle.id}`}>
+//                           <button className="btn default-button">
+//                             Update your information
+//                           </button>
+//                         </Link>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//               )}
+//             </div>
+//           </div>
+//         </div>
+//       </>
+//     );
+//   } else if (userStatus === "failed") {
+//     content = <div>Error loading user data.</div>;
+//   }
+
+//   return content;
+// };
+
+// export default MyAccount;
--- src/Components/App.js
+++ src/Components/App.js
@@ -55,3 +55,62 @@ const App = ()=> {
 };
 
 export default App;
+
+
+// import React, { useEffect } from 'react';
+// import Home from './Home';
+// import Favorite from './Favorite';
+// import Navbar from './Navbar';
+// import Map from './Map';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { loginWithToken, fetchFavorite } from '../store';
+// import { Link, Routes, Route } from 'react-router-dom';
+// import MyAccount from './Account';
+// import About from './About';
+// import UpdateUserForm from './UpdateUserForm';
+// import UpdateVehicleForm from './UpdateVehicleForm';
+// // require('dotenv').config({path: '../.env'})
+
+
+// const App = ()=> {
+//   const { auth } = useSelector(state => state);
+//   const dispatch = useDispatch();
+//   useEffect(()=> {
+//     dispatch(loginWithToken());
+//   }, []);
+
+//   useEffect(()=> {
+//     if(auth.id){
+//       dispatch(fetchFavorite());
+//     }
+//   }, [auth]);
+  
+//   return (
+//     <div>
+//       <Navbar />
+//       <div>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/map/place/:address/:stationId" element={<Map />} />
+//           <Route path="/map/place/:address" element={<Map />} />
+//           <Route path="/map/dir/:startAddress/:endAddress" element={<Map />} />
+//           <Route path="/about" element={<About />} />
+//         </Routes>
+//         {
+//         !!auth.id  && (        
+//           <div>
+//             <Routes>
+//               <Route path='/myaccount/updateuser' element={<UpdateUserForm />} />
+//               <Route path='/myaccount/updatevehicle' element={<UpdateVehicleForm />} />
+//               <Route path='/myaccount/updatevehicle/:id' element={<UpdateVehicleForm />} />
+//               <Route path="/myaccount" element={<MyAccount />} />
+//             </Routes>
+//           </div>
+//         )
+//       }
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
